7964940b7e734389bb899f07376e6b2e65d0518b
Sakura <Sakura-039@users.noreply.github.com>
2026-08-19 22:57:20 +0800

    Site updated: 2026-08-19 22:57:02

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1787064113461";const preCache = ["/images/taichi.png","/images/banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1787151381313";const preCache = ["/images/taichi.png","/images/banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
